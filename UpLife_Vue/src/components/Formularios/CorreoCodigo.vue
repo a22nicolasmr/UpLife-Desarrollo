800c3cd84f7527e50b32c09d40e8e536f7e8e397
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-05-20 10:50:33 +0200

    csrf token

--- UpLife_Vue/src/components/Formularios/CorreoCodigo.vue
+++ UpLife_Vue/src/components/Formularios/CorreoCodigo.vue
@@ -7,20 +7,20 @@ export default {
     };
   },
   methods: {
-    // getCookie(name) {
-    //   let cookieValue = null;
-    //   if (document.cookie && document.cookie !== "") {
-    //     const cookies = document.cookie.split(";");
-    //     for (let i = 0; i < cookies.length; i++) {
-    //       const cookie = cookies[i].trim();
-    //       if (cookie.startsWith(name + "=")) {
-    //         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-    //         break;
-    //       }
-    //     }
-    //   }
-    //   return cookieValue;
-    // },
+    getCookie(name) {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          if (cookie.startsWith(name + "=")) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
+    },
 
     //comprobar se o correo introducido é válido
     async comprobarCorreo(event) {
@@ -63,7 +63,7 @@ export default {
             headers: {
               "Content-Type": "application/json",
             },
-            // "X-CSRFToken": getCookie("csrftoken"),
+            "X-CSRFToken": getCookie("csrftoken"),
             body: JSON.stringify({
               email: this.identificador,
               codigo: codigo,
